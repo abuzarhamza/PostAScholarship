dbd70ef2e8117a0d6a6bc383ab785a04177a40a1
abuzar hamza <abuzarhamza@gmail.com>
2014-03-13 14:28:42 +0530

    added sub to remove comment from SQL statement

--- automation/perl/create_table.pl
+++ automation/perl/create_table.pl
@@ -17,7 +17,7 @@ my $filePath    = '../../mysql/'. $sqlFileName;
 
 print "Starting the script\n";
 
-my $dbh = DBI->connect('dbi:mysql:postascholarship_db:localhost:3306;mysql_socket=/opt/lampp/var/mysql/mysql.sock','root','') 
+my $dbh = DBI->connect('dbi:mysql:postascholarship_db:localhost:3306;mysql_socket=/opt/lampp/var/mysql/mysql.sock','root','', {PrintError => 1 , RaiseError =>1}) 
         or die "Unable to connect: $DBI::errstr\n";
 
 
@@ -26,11 +26,35 @@ while(<$fh1>) {
 
     my $query =  $_;
     print "QUERY : $query\n";
-
-    my $sh    = $dbh->prepare($query) or die "";
-    $sh->execute();
-  
+    $query = satantize_query($query);
+    print "AFTER SANITIZATION : $query\n";
+    
+    #my $sh    = $dbh->prepare($query);
+    #$sh->execute();
+    #$sh->finish(); 
 }
 close $fh1;
 
+$dbh->disconnect() or warn "Disconnection failed : $DBI::errstr";
 
+
+=head
+this functional will remove the comment in the query statement
+=cut
+sub satantize_query {
+    my ($query) = @_;
+    my $tmpSQL  = ""; 
+    foreach (split(/\n/,$query)) {
+	if ($_=~/^--/) {
+		next;
+	}
+        elsif ($_=~/(.+)--.+/) {
+	     $tmpSQL .= $1."\n";
+	}
+        else {
+		$tmpSQL .= $_."\n"
+	}
+    }
+   
+    return $tmpSQL;	
+}
