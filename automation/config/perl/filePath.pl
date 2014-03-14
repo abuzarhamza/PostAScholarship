075e8114a4dde207f7bbd4e15221fd63f7c211d0
abuzarhamza <abuzarhamza@gmail.com>
2014-03-14 22:12:22 +0530

    added the module file path

--- automation/config/perl/filePath.pl
+++ automation/config/perl/filePath.pl
@@ -5,6 +5,8 @@ use strict;
 
 our %CONFIG = ();
 
+$CONFIG{'PERL_MODULE_PATH'} = '../modules';
+
 $CONFIG{'SQL_DIRPATH'}      = '../../mysql/';
 $CONFIG{'SQL_CRDB_FILE'}    = 'createDB.sql';
 $CONFIG{'SQL_CRTABLE_FILE'} = 'createTable.sql';
