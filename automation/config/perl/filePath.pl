64e8d19fd2cb66c9364dd2043159a3964a6f1649
abuzarhamza <abuzarhamza@gmail.com>
2014-03-14 20:15:54 +0530

    no signidficant change

--- automation/config/perl/filePath.pl
+++ automation/config/perl/filePath.pl
@@ -6,5 +6,5 @@ use strict;
 our %CONFIG = ();
 
 $CONFIG{'SQL_DIRPATH'}      = '../../mysql/';
-$CONFIG{'SQL_CRTABLE_FILE'} = 'createTable.sql';
 $CONFIG{'SQL_CRDB_FILE'}    = 'createDB.sql';
+$CONFIG{'SQL_CRTABLE_FILE'} = 'createTable.sql';
