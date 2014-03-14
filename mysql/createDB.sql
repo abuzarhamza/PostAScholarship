670a4eca3401f9f5e75a3c3db0445c53af873ea2
abuzar hamza <abuzarhamza@gmail.com>
2014-03-14 16:49:05 +0530

    added the database file

---
+++ mysql/createDB.sql
@@ -0,0 +1,5 @@
+create database if not exists postascholarship_db 
+charcterset utf8 collate utf8_general_ci;
+
+create database if not exists postascholarship_db_arch 
+charcterset utf8 collate utf8_general_ci;
