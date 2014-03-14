341ada0a484afd91a5356d5cd18ab681537edd2e
abuzarhamza <abuzarhamza@gmail.com>
2014-03-14 22:12:53 +0530

    minnor changes.

--- automation/perl/script/dbAutomation.pl
+++ automation/perl/script/dbAutomation.pl
@@ -4,13 +4,13 @@ use warnings;
 use strict;
 use DBI;
 use DBD::mysql;
-use lib 'perl/modules';
+use Data::Dumper;
+use lib '../modules';
+use SQL::Sanity qw(clearComment);
 
-require '../config/perl/filePath.pl';
+require '../../config/perl/filePathConf.pl';
 
-use SQL::Sanity (clearComment);
 #use SQL::General (readSQLFile);
-
 
 ##########################
 #TO DO
@@ -18,7 +18,11 @@ use SQL::Sanity (clearComment);
 #function readSQLFile read the file and return the array with the list of query
 ##########################
 
+
 our %CONFIG;
+
+
+
 
 CreateDatabase(\%CONFIG);
 CreateTable(\%CONFIG);
@@ -35,10 +39,10 @@ sub CreateDatabase {
 
     local $\ = ';';
 
-    my $sqlFilePath = $Config{'SQL_DIRPATH'};
-    my $sqlFileName = $sqlFileName . $Config{'SQL_CRDB_FILE'};
+    my $baseDir = $Config{'SQL_DIRPATH'};
+    my $sqlFileName = $baseDir  . $Config{'SQL_CRDB_FILE'};
 
-    my $dh = DBI->connect('dbi:mysql:;mysql_socket=/opt/lampp/var/mysql/mysql.sock','root','');
+    my $dbh = DBI->connect('dbi:mysql:;mysql_socket=/opt/lampp/var/mysql/mysql.sock','root','',{PrintError=>1, RaiseError=>1});
 
     open(my $fh, "$sqlFileName") or die "cant open the file : $!";
     while (<$fh>) {
@@ -72,10 +76,10 @@ sub CreateTable {
 
     local $\ = ';';
 
-    my $sqlFilePath = $Config{'SQL_DIRPATH'};
-    my $filePath    = $sqlFilePath . $Config{'SQL_CRTABLE_FILE'};
+    my $baseDir  = $Config{'SQL_DIRPATH'};
+    my $filePath = $baseDir . $Config{'SQL_CRTABLE_FILE'};
 
-    my $dh = DBI->connect('dbi:mysql:postascholarship_db;mysql_socket=/opt/lampp/var/mysql/mysql.sock','root','');
+    my $dbh = DBI->connect('dbi:mysql:postascholarship_db;mysql_socket=/opt/lampp/var/mysql/mysql.sock','root','',{PrintError=>1, RaiseError=>1});
     open(my $fh, "$filePath") or die "cant open the file : $!";
     while (<$fh>) {
         chomp;
