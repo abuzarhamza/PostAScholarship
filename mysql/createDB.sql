9c78857dfb5f0308d5d8e4903a7e181c48b55f99
abuzar hamza <abuzarhamza@gmail.com>
2014-03-14 16:57:09 +0530

    syntex correction

--- mysql/createDB.sql
+++ mysql/createDB.sql
@@ -1,5 +1,8 @@
 create database if not exists postascholarship_db 
-charcterset utf8 collate utf8_general_ci;
+default character set utf8 
+default  collate utf8_general_ci;
+
 
 create database if not exists postascholarship_db_arch 
-charcterset utf8 collate utf8_general_ci;
+default character set utf8 
+default  collate utf8_general_ci;
