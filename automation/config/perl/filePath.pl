f0cb4523cafb1a4b132d17c9124cde7234362bfb
abuzar hamza <abuzarhamza@gmail.com>
2014-03-14 16:46:00 +0530

    added the file path

---
+++ automation/config/perl/filePath.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+our %CONFIG = ();
+
+$CONFIG{'SQL_DIRPATH'}      = '../../mysql/';
+$CONFIG{'SQL_CRTABLE_FILE'} = 'createTable.sql';
+$CONFIG{'SQL_CRDB_FILE'}    = 'createDB.sql';
