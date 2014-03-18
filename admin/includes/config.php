5ab9e42caea65d0708cc3229ed989f792a363c2c
abuzarhamza <abuzarhamza@gmail.com>
2014-03-18 23:11:39 +0530

    added the config file

---
+++ admin/includes/config.php
@@ -0,0 +1,7 @@
+<?php
+	define('DB_SERVER','localhost'); //hostname
+	define('DB_SERVER_USERNAME','root'); //database username
+	define('DB_SERVER_PASSWORD',''); //database password
+	define('DB_DATABASE','postascholarship_db'); //database name
+	define('SITE_URL','http://localhost/PostAScholarship') // site url
+?>
