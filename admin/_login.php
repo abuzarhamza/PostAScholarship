f8766dc4bac5f164a8945d3198d197a5e05527b5
abuzar hamza <abuzarhamza@gmail.com>
2014-03-21 02:28:30 +0530

    admin login page

--- admin/_login.php
+++ admin/_login.php
@@ -1,11 +1,8 @@
 <div class="container">
     <form class="form-signin" role="form" method="post" action="admin_login.php?action=login" name ="adminlogin" id="adminlogin">
       <h2 class="form-signin-heading">Admin Login</h2>
-      <input type="email" class="form-control" placeholder="Email address" required autofocus>
+      <input type="username" class="form-control" placeholder="User name" required autofocus>
       <input type="password" class="form-control" placeholder="Password" required>
-      <label class="checkbox">
-        <input type="checkbox" value="remember-me"> Remember me
-      </label>
       <button class="btn btn-lg btn-primary btn-block" type="submit" action="">Sign in</button>
     </form>
 </div> 
