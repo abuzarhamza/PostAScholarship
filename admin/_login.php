7997523d3ee41c20febe56175e775a822e6716f8
abuzar hamza <abuzarhamza@gmail.com>
2014-03-20 18:53:07 +0530

    login script.

--- admin/_login.php
+++ admin/_login.php
@@ -1,6 +1,6 @@
 <div class="container">
-    <form class="form-signin" role="form">
-      <h2 class="form-signin-heading">Please sign in</h2>
+    <form class="form-signin" role="form" method="post" action="admin_login.php?action=login" name ="adminlogin" id="adminlogin">
+      <h2 class="form-signin-heading">Admin Login</h2>
       <input type="email" class="form-control" placeholder="Email address" required autofocus>
       <input type="password" class="form-control" placeholder="Password" required>
       <label class="checkbox">
