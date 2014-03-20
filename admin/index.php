c1babd50dd28cc3ed907ca24032e4f121d996593
abuzar hamza <abuzarhamza@gmail.com>
2014-03-21 02:28:55 +0530

    cosmetic changes

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,8 @@
 <?php
     ob_start();
     session_start();
-    require_once("includes/config.php");
-    require_once("includes/functions.php");
+    require_once("./includes/config.php");
+    require_once("./includes/functions.php");
 ?>
 
 <!DOCTYPE html>
