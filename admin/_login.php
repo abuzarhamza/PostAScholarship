24560592eb82f38464aa9049be85e52a1cfb9aa6
abuzar hamza <abuzarhamza@gmail.com>
2014-03-23 03:43:30 +0530

    added the bootstrap msg alert for login check

--- admin/_login.php
+++ admin/_login.php
@@ -1,8 +1,30 @@
-<div class="container">
-	<div class="alert" id="validation_msg">                  
-    </div>
+<?php if($RES=="login_error") { ?>
 
-    <form class="form-signin" role="form" method="post" action="./admin_login.php?action=login" name ="adminlogin" id="adminlogin">
+    <div class="alert alert-danger">
+        <a class="close" data-dismiss="alert">×</a>
+        <strong>Error!</strong> Please enter the valid Username and Password.
+    </div>
+<? } ?>
+
+<?php if ($RES=="logout_success") { ?>
+    <div class="alert alert-success">
+        <a class="close" data-dismiss="alert">×</a>
+    <strong>Success!</strong> You have logout successfully.
+    </div>
+<? } ?>
+
+
+<? if($RES=="change_password_success") { ?>
+    <div class="alert alert-info">
+      <a class="close" data-dismiss="alert">×</a>
+      <strong>Info!</strong> Your Password changed successfully<br>Please login again with your new Password.
+    </div>
+<? } ?>
+
+<div class="container">
+    <div id="validation_msg" class="hide"></div>
+
+    <form class="form-signin" role="form" method="post" action="./admin_login.php?action=login" name ="adminlogin" id="adminlogin" onSubmit="return check_form();">
       <h2 class="form-signin-heading">Admin Login</h2>
       <input type="username" class="form-control" placeholder="User name" id ="username" name="username" required autofocus>
       <input type="password" class="form-control" placeholder="Password" id ="password" name="password" required>
@@ -11,59 +33,33 @@
 </div>
 
 <script language="Javascript" type="text/javascript">
-$(document).ready(function(){
+function check_form()
+{
+    form=document.frmLogin;
+    error=0;
+    msg="";
+    if(trim(form.username.value)==null)
+    {
+        error=1;
+        msg+='&bull; Please enter Username<br>';
+    }
+    if(trim(form.password.value)==null)
+    {
+        error=1;
+        msg+='&bull; Please enter Password<br>';
+    }
 
-	$("#adminlogin").submit(function(){
-	        form=document.adminlogin;
-	        error=0;
-	        msg="";
-	        
-	        if( $("#username").val()==null || $("#username").val()=="")
-	        {
-	            error=1;
-	            msg+='&bull; Please enter user name.<br>';
-	        }
-	        if($("#password").val()==null || $("#password").val()=="")
-	        {
-	            error=1;
-	            msg+='&bull; Please enter password<br>';
-	        }
-	        
-	        //return false;
-	        if(error==1)
-	        {
-	            document.getElementById("validation_msg").innerHTML=msg;
-	            //alert(msg);
-	            $(".alert").show();
-	            return false;
-	        }
-	        else
-	        {
-	           
-	            $(".loading").show();
-	            
-	            $.ajax({
-	                url: 'center_ops.php?action=login',
-	                data: $("#adminlogin").serialize(),
-	                type: "POST",
-	                success: function(response){
-	                    if(response!='Success'){
-	                        alert(response);
-	                        
-	                    }
-	                    else{
-	                        
-	                            redirect("center_account.php");
-	                       
-	                    }
-	                    
-	                    $(".loading").hide();
-	                }
-	            });
-	            
-	            return false;
-	        }
-	    });
+    if(error==1)
+    {
+        document.getElementById("validation_msg").className="validation";
+        document.getElementById("validation_msg").innerHTML=msg;
 
-});
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 </script> 
