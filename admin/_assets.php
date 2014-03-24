8ad8ad48f08823d71921c1ecbf9cc8efd22f25d5
abuzarhamza <abuzarhamza@gmail.com>
2014-03-25 01:20:31 +0530

    included the new new css

--- admin/_assets.php
+++ admin/_assets.php
@@ -1,4 +1,5 @@
 	<link rel="stylesheet" href="templates/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="templates/font-awesome/css/font-awesome.min.css">
 	<style>
 		body { padding-top: 70px; }
 	</style>
