23be9aacb1b448853a2bc853d2ab9e3e63d74aa4
abuzarhamza <abuzarhamza@gmail.com>
2014-03-28 23:19:31 +0530

    added the manage admin detail

---
+++ admin/manage_admin_details.php
@@ -0,0 +1,21 @@
+<?php
+    include("./_admin_start.php");
+    include("./_admin_initialize.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title><? echo SITE_URL_TITLE; ?></title>
+<? include "_assets.php";?>
+</head>
+
+
+
+<?php
+    include("_footer.php");
+    include ("_assets_javascript.php");
+?>
+</div>
+</body>
