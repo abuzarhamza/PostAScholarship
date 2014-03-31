6b91226e0a75c5ede91e8a408b261f869b160aa5
abuzar hamza <abuzarhamza@gmail.com>
2014-03-31 22:04:00 +0530

    added the new store proc for update

--- mysql/plsql/admin/StoreProc.sql
+++ mysql/plsql/admin/StoreProc.sql
@@ -25,5 +25,20 @@ begin
 end; $$
 
 
+create procedure verify_and_update_adminprofile(in_user_name varchar(30), in_display_name varchar(30),
+    in_first_name varchar(30),in_last_name varchar(30),in_about_me varchar(250),in_web varchar(250))
+begin
+    if (select user_name from user_profile where user_name = in_user_name ) then
+        update user_profile set display_name = display_name,
+                                first_name = in_first_name,
+                                last_name = in_last_name,
+                                about_me = in_about_me,
+                                web = in_web
+         where user_name  = in_user_name;
+        select '1';
+    else
+        select 'does not exists';
+    end if;
+end; $$
 
 delimiter ;
