12b73b3a3cb937ce758cd91b6bb427486dca2675
abuzar hamza <abuzarhamza@gmail.com>
2014-03-31 17:45:33 +0530

    added the form to update the user profile

--- admin/profile_manager.php
+++ admin/profile_manager.php
@@ -35,8 +35,61 @@
 <?php
     //STORE proredur
     //give result and create and id too if not
-    $sql = "CALL verify_and_insert_usrename('admin')";
+    $userName = $_SESSION['s_admin_username'];
+    $sql = "CALL verify_and_insert_username('$userName')";
+    $result   = mysql_query($sql);
+
+    if ( $result ) {
+        $arrRes   = mysql_fetch_assoc($result);
+    }
 ?>
+
+<?php if ($result) : ?>
+        <form class="form-horizontal" role="form" action="./admin_login.php?action=save_admin_settings" method="post">
+            <div class="form-group">
+                <label for="display_name" class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-md-offset-1 col-lg-offset-1"> Display Name</label>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                  <input type="text" class="form-control" id="display_name" name="display_name" maxlength="30" value="<?=$arrRes['display_name']; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="first_name" class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-md-offset-1 col-lg-offset-1"> First Name</label>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                  <input type="text" class="form-control" id="first_name" name="first_name"  maxlength="30" value="<?=$arrRes['first_name']; ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="last_name" class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-md-offset-1 col-lg-offset-1"> Last Name</label>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                  <input type="text" class="form-control" id="last_name" name="last_name"  maxlength="30" value="<?=$arrRes['last_name']; ?>">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="web" class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-md-offset-1 col-lg-offset-1"> Web url</label>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                  <input type="text" class="form-control" id="web" name="web"  maxlength="30" value="<?=$arrRes['web']; ?>">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="about_me" class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-md-offset-1 col-lg-offset-1"> About me</label>
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                  <textarea class="form-control" id="about_me" name="about_me" rows="4" cols="40" maxlength="140">
+                    <?=$arrRes['about_me']; ?> </textarea>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 col-md-offset-3 col-lg-offset-3">
+                  <button type="submit" class="btn btn-primary">Update profile</button>
+                </div>
+            </div>
+        </form>
+<? else : ?>
+
+<? endif; ?>
+
 <?php
     include("_footer.php");
     include ("_assets_javascript.php");
