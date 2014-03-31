759cc4a3eb676dfcc4ccc96b80a8c5e402e6d1df
abuzar hamza <abuzarhamza@gmail.com>
2014-03-31 17:45:04 +0530

    corrected the typo error

--- mysql/plsql/admin/StoreProc.sql
+++ mysql/plsql/admin/StoreProc.sql
@@ -1,17 +1,6 @@
 delimiter $$
 
-create procedure verify_and_insert_usrename(in_user_name varchar(30) )
-begin
-    if (select id from user_profile where user_name = in_user_name ) then
-        select id,user_name,display_name,first_name,last_name,type,about_me,web
-          from user_profile where user_name =  in_user_name;
-    else
-        insert into user_profile(user_name) values(in_user_name);
-        select id,user_name,display_name,first_name,last_name,type,about_me,web 
-          from user_profile where user_name = in_user_name;
-    end if;
-end; $$
-
+drop procedure if exists varify_username;
 create procedure varify_username(in_user_name varchar(30))
 begin
     if ( select id from user_profile where user_name = in_user_name ) then
@@ -20,5 +9,21 @@ begin
         select 'does not exists';
     end if;
 end; $$
+
+
+drop procedure if exists verify_and_insert_username;
+create procedure verify_and_insert_username(in_user_name varchar(30) )
+begin
+    if (select id from user_profile where user_name = in_user_name ) then
+        select id,display_name,first_name,last_name,about_me,web
+          from user_profile where user_name =  in_user_name;
+    else
+        insert into user_profile(user_name) values(in_user_name);
+        select id,display_name,first_name,last_name,about_me,web
+          from user_profile where user_name = in_user_name;
+    end if;
+end; $$
+
+
 
 delimiter ;
