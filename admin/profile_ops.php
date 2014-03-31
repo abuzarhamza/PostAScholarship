f7b0f298ced0262c385756075f704ea7503365ee
abuzar hamza <abuzarhamza@gmail.com>
2014-03-31 22:06:19 +0530

    profile manager file

---
+++ admin/profile_ops.php
@@ -0,0 +1,68 @@
+<?php
+ob_start();
+session_start();
+header("Cache-control: private");
+
+require_once("./includes/config.php"); //include configuration file
+require_once("./includes/functions.php"); //include file containing all functions
+
+$conn = mysql_connect(DB_SERVER,DB_SERVER_USERNAME,DB_SERVER_PASSWORD);
+mysql_select_db(DB_DATABASE) or die("database not available");
+//mysqli_set_charset($conn,"utf8");
+?>
+
+<?
+    if  ( array_key_exists('action', $_GET) ) {
+        $action           = $_GET['action'];
+        $keyForUpdateForm = "display_name,first_name,last_name,about_me,web";
+        $userName         = $_SESSION['s_admin_username'];
+        $errorMsg         = "";
+        $errorFlag        = 0;
+
+        if ( $action == "update_user_profile" ) {
+
+            foreach (explode(",", $keyForUpdateForm) as $key) {
+                if ( array_key_exists($key, $_POST) ) {
+                    $_POST[$key] = mysql_real_escape_string(trim($_POST[$key]));
+
+                }
+                else {
+                    $errorFlag = 1;
+                }
+            }
+
+            if ( $errorFlag != 1 ) {
+                $sql      = "CALL verify_and_update_adminprofile('$userName','$_POST[display_name]','$_POST[first_name]','$_POST[last_name]','$_POST[about_me]','$_POST[web]')";
+                $result   = mysql_query($sql);
+                //$values   = mysql_fetch_array($result);
+                //echo '<p>'.var_dump($values). '<p>';
+                if ($result != 1) {
+                    //echo "<P> $sql </p> <p>\$result : $result</p><p> \$conn : $conn<p>".mysql_error();
+                    $errorFlag = 1;
+                }
+            }
+
+        }
+        else {
+            $errorFlag = 1;
+        }
+        //echo "<p>I ma gereSS</p>";
+        if ($errorFlag  == 1)  {
+            //echo "<p>should forward</p>";
+            header("Location: profile_manager.php?res=update_error");
+            exit;
+        }
+        else {
+            header("Location: profile_manager.php?res=update_success");
+            exit;
+        }
+    }
+    else {
+        header("Loaction : profile_manager.php?res=update_error");
+        exit;
+    }
+?>
+
+<?php
+ob_end_flush();
+?>
