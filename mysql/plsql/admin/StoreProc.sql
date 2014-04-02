c15e6249640a5aa140fe31e43b4db072a9abd60c
abuzar hamza <abuzarhamza@gmail.com>
2014-04-02 11:52:21 +0530

    minnor changes in stored proc

--- mysql/plsql/admin/StoreProc.sql
+++ mysql/plsql/admin/StoreProc.sql
@@ -41,16 +41,21 @@ begin
     end if;
 end; $$
 
-
-
-create procedure get_postview_for_admin(in_offset int,in_max_row int)
+create procedure get_count_posttype(in_post varchar(30))
 begin
-    if (select count(id) from post where post_type = "POST") then
-        select p.title as title,u.user_name as title,p.creation_date as creation_date,p.lastedit_date as lastedit_date,p.post_type as post_type,p.book_count as book_count,p.view as view
+    select count(id) from post
+     where post_type = in_post
+       and root = 1;
+end; $$
+
+create procedure get_postview_for_admin(in_offset int,in_max_row int,in_post_type varchar(30))
+begin
+    if (select count(id) from post where post_type = in_post_type) then
+        select p.title as title,u.user_name as user_name,p.creation_date as creation_date,p.lastedit_date as lastedit_date,p.post_type as post_type,p.book_count as book_count,p.view as view
           from post p
           inner join user_profile u
          on p.author_id = u.id
-         and p.post_type = "POST"
+         and p.post_type = in_post_type
          and p.root = 1
          order by creation_date
          limit in_offset,in_max_row;
@@ -59,11 +64,23 @@ begin
     end if;
 end; $$
 
-create procedure get_count_posttype(in_post varchar(30))
-begin
-    select count(id) from post
-     where post_type = in_post
-       and root = 1
-end;
 
+
+create procedure get_postviewtags_for_admin(in_offset int,in_max_row int,in_post_type varchar(30))
+begin
+    if (select count(id) from post where post_type = in_post_type) then
+        select p.title as title,u.user_name as user_name,p.creation_date as creation_date,p.lastedit_date as lastedit_date,p.post_type as post_type,p.book_count as book_count,p.view as view
+          from post p
+         inner join user_profile u
+            on p.author_id = u.id
+           and p.post_type = in_post_type
+           and p.root = 1
+         order by p.creation_date
+         limit in_offset,in_max_row;
+    else
+        select '0';
+    end if;
+end; $$
 delimiter ;
+
+--select tag_name from tag t where t.id in (select tag_id from tag_post_rel where post_id = p.id),
