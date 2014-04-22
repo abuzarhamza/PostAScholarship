2b748bf6baadd3394372c5f2c07bb5fc43cd79f2
abuzar hamza <abuzarhamza@gmail.com>
2014-04-22 16:05:13 +0530

    added the script to add the post.

---
+++ admin/add_post.php
@@ -0,0 +1,39 @@
+<?php
+    ob_start();
+    session_start();
+    require_once("./includes/config.php");
+    require_once("./includes/functions.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<link rel="icon" href="templates/img/logo.png" sizes="16x16" type="image/png">
+<title><? echo SITE_URL_TITLE; ?> - Administrator</title>
+<?php include "_assets.php";?>
+</head>
+<?php
+    include("_top.php");
+?>
+
+
+<div="container" style="margin: 10px;">
+    <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1" >
+                <h1>Add Post</h1>
+            </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1" >
+            <ol class="breadcrumb">
+              <li><a href="./index.php?page=home">Home</a></li>
+              <li><a href="./index.php?page=dashboard">Dashboard</a></li>
+              <li><a href="./post.php">Manage Post</a></li>
+              <li class="active">Add post</li>
+            </ol>
+        </div>
+    </div>
+
+    
