ecf0e94d1af335b57c4823d4f6347797478e3984
abuzar hamza <abuzarhamza@gmail.com>
2014-04-23 01:45:44 +0530

    testing the diaplay table for post.

--- admin/post.php
+++ admin/post.php
@@ -3,6 +3,7 @@
     session_start();
     require_once("./includes/config.php");
     require_once("./includes/functions.php");
+    require_once("./classes/PHP-DataPagination/DataPageset.class.php");
 ?>
 
 <!DOCTYPE html>
@@ -37,32 +38,105 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 col-md-offset-1 col-lg-offset-1" >
             <div class="span2">
-              <p><a href="./add_post.php"><button class="btn btn-warning"><span class="fa fa-pencil-square-o"> </span> Add post</button></p>
+              <p><a href="./add_post.php"><button class="btn btn-warning"><span class="fa fa-pencil-square-o"> </span> Add post</button></a></p>
             </div>
         </div>
     </div>
 <?
 
-        $sql         = "CALL get_count_posttype('POST')";
-        $result      = mysql_query($sql);
-        $pageHtml    = "";
-        $postHtml    = "<tr>
-                          <td>#</td>
-                          <td>title</td>
-                          <td>author name</td>
-                          <td>creation date</td>
-                          <td>view,bookmark stats</td>
-                        </tr>\n";
-        $errorFlag   = 0;
+    $sql         = "CALL get_count_posttype('POST')";
+    $result      = mysql_query($sql);
+    $pageHtml    = "";
+    $postHtml    = "<tr>
+                      <td>#</td>
+                      <td>title</td>
+                      <td>author name</td>
+                      <td>creation date</td>
+                      <td>view,bookmark stats</td>
+                    </tr>\n";
+    $errorFlag   = 0;
 
+    if ( mysql_error() ) {
+        $errorFlag =1;
+    }
+
+    $postCount    = mysql_result($result, 0);
+
+
+    if ( $postCount == 0 ) {
+      $errorFlag = 2;
+    }
+    else {
+        $recLimit     = 10;
+        $currentPage  = 1;
+
+        if ( isset( $_GET{'rec'} ) ) {
+            $recLimit    = $_GET{'rec'};
+        }
+        if ( isset( $_GET{'page'} ) ) {
+            $currentPage = $_GET{'page'};
+        }
+
+        $pageObj      =  new DataPageset($postCount,$recLimit,$currentPage,5,'slide');
+        if ( $pageObj->current_page == 1 ) {
+          $offset     = 0;
+        }
+        else {
+          $offset    =  $pageObj->current_page * $recLimit;
+        }
+        $sql         = "CALL get_postview_for_admin($offset,$recLimit,'POST')";
+        $result      = mysql_query($sql);
         if ( mysql_error() ) {
             $errorFlag =1;
         }
+        else {
+            echo "$offset,$recLimit \$result : $result \$postCount : $postCount \$row : $row";
+            while ( $row = mysql_fetch_array($result) ) {
+                $postHtml .= '<tr>
+                              <td><a href="">'.$row['title'].'</a></td>
+                              <td><a href="">'.$row['author name'].'</a></td>
+                              <td>'.$row['creation_date'].'</td>
+                                <td> <span class="glyphicon glyphicon-bookmark"> <span class="badge"> '.$row['book_count'] .'</span>
+                                   <span class=" fa fa-caret-square-o-right"> <span class="badge"> '. $row['view']."</span>
+                              </td>
+                            </tr>\n";
+            }
+
+            //pagination
+            $pageHtml .= '<div class="row">  <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1" >';
+            $pageHtml .= '<ul class="pagination">';
+
+            if ($pageObj->current_page == 1 ) {
+              $pageHtml .= '<li class="disabled">&laquo</li>';
+            }
+            else {
+              $pageHtml .= '<li><a href="'.$_PHP_SELF.'?page='.$pageObj->previous_page().'&rec=10'.'">&laquo;</a></li>';
+            }
+
+            foreach ($pageObj->page_set_pages as $pageNumber) {
+
+                if ( $pageNumber == $pageObj->current_page ) {
+                $pageHtml .= '<li class="active"><span>'.$pageNumber.'<span class="sr-only">(current)</span></span></li>';
+                }
+                else {
+                $pageHtml .= '<li><a href="'.$_PHP_SELF.'?page='.$pageNumber.'&rec=10'.'">'.$page.'</a></li>';
+                }
+            }
+
+            if ( $pageObj->current_page == $pageObj->last_page()) {
+                $pageHtml .= '<li class="disabled">&raquo;</li>';
+            }
+            else {
+                $pageHtml .= '<li><a href="'.$_PHP_SELF.'?page='.$pageObj->next_page().'&rec=10'.'">&raquo;</a></li>';
+            }
+
+            $pageHtml .= '</div> </div>';
 
 
-        $postCount    = mysql_result($result, 0);
-
-        /*
+        }
+        
+    }
+/*
 **********SAMPLE USAGE *************
 $obj = new DataPageset(500,10,4,5,'slide');
 echo "test1\n";
@@ -85,80 +159,14 @@ foreach ($obj->page_set_pages as $v) {
     echo "$v\n";
 }
 */
-    if ( $postCount == 0 ) {
-      $errorFlag = 2;
-    }
-    else {
-        $recLimit     = 10;
-        $currentPage  = 1;
-
-        if ( isset( $_GET{'rec'} ) ) {
-            $recLimit    = $_GET{'rec'};
-        }
-        if ( isset( $_GET{'page'} ) ) {
-            $currentPage = $_GET{'page'};
-        }
-
-        $pageObj      =  new DataPageset($postCount,$recLimit,$currentPage,5,'slide');
-        if ( $pageObj->current_page == 1 ) {
-          $offset      = 0;
-        }
-        else {
-          $offset      =  $pageObj->current_page * $recLimit;
-        }
-        $sql         = "CALL get_postviewtags_for_admin($offset,$recLimit,'POST')";
-        $result      = mysql_query($sql);
-        while ( $row = mysql_fetch_array ($result)) {
-                $postHtml .= '<tr>
-                              <td><a href="">'.$row['title'].'</a></td>
-                              <td><a href="">'.$row['author name'].'</a></td>
-                              <td>'.$row['creation_date'].'</td>
-                                <td> <span class="glyphicon glyphicon-bookmark"> <span class="badge"> '.$row['book_count'] .'</span>
-                                   <span class=" fa fa-caret-square-o-right"> <span class="badge"> '. $row['view']."</span>
-                              </td>
-                            </tr>\n";
-        }
-
-        //pagination
-        $pageHtml .= '<div class="row">  <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1" >';
-        $pageHtml .= '<ul class="pagination">';
-
-        if ($pageObj->current_page == 1 ) {
-          $pageHtml .= '<li class="disabled">&laquo</li>';
-        }
-        else {
-          $pageHtml .= '<li><a href="'.$_PHP_SELF.'?page='.$pageObj->previous_page().'&rec=10'.'">&laquo;</a></li>';
-        }
-
-        foreach ($pageObj->page_set_pages as $pageNumber) {
-
-             if ( $pageNumber == $pageObj->current_page ) {
-                $pageHtml .= '<li class="active"><span>'.$pageNumber.'<span class="sr-only">(current)</span></span></li>';
-             }
-             else {
-                $pageHtml .= '<li><a href="'.$_PHP_SELF.'?page='.$pageNumber.'&rec=10'.'">'.$page.'</a></li>';
-             }
-        }
-
-        if ( $pageObj->current_page == $pageObj->last_page()) {
-            $pageHtml .= '<li class="disabled">&raquo;</li>';
-        }
-        else {
-            $pageHtml .= '<li><a href="'.$_PHP_SELF.'?page='.$pageObj->next_page().'&rec=10'.'">&raquo;</a></li>';
-        }
-
-        $pageHtml .= '</div> </div>';
-
-    }
-
 ?>
 
     <!--error msg-->
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1" >
         <?
-            if ($RES == "sqlerror" || $errorFlag==1 ) echo '<div class="alert alert-danger alert-dismissable">SQL error</div>';
-            if ($RES == "nopost" ||  $errorFlag==2) echo '<div class="alert alert-danger alert-dismissable">No post can be found</div>';
+            if ($RES == "sqlerror" || $errorFlag == 1 ) echo '<div class="alert alert-danger alert-dismissable">SQL error</div>';
+            if ($RES == "nopost" ||  $errorFlag == 2) echo '<div class="alert alert-danger alert-dismissable">No post can be found</div>';
         ?>
         </div>
     </div>
