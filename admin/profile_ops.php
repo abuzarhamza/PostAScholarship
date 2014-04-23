ed37b4ec81a9dd3ccd49899db8559d68e99f2fc1
abuzar hamza <abuzarhamza@gmail.com>
2014-04-23 15:56:47 +0530

    changes as per the new mysqli

--- admin/profile_ops.php
+++ admin/profile_ops.php
@@ -6,8 +6,8 @@ header("Cache-control: private");
 require_once("./includes/config.php"); //include configuration file
 require_once("./includes/functions.php"); //include file containing all functions
 
-$conn = mysql_connect(DB_SERVER,DB_SERVER_USERNAME,DB_SERVER_PASSWORD);
-mysql_select_db(DB_DATABASE) or die("database not available");
+// $conn = mysql_connect(DB_SERVER,DB_SERVER_USERNAME,DB_SERVER_PASSWORD);
+// mysql_select_db(DB_DATABASE) or die("database not available");
 //mysqli_set_charset($conn,"utf8");
 ?>
 
@@ -23,7 +23,7 @@ mysql_select_db(DB_DATABASE) or die("database not available");
 
             foreach (explode(",", $keyForUpdateForm) as $key) {
                 if ( array_key_exists($key, $_POST) ) {
-                    $_POST[$key] = mysql_real_escape_string(trim($_POST[$key]));
+                    $_POST[$key] =  $conn->mysqli_real_escape_string(trim($_POST[$key]));
                 }
                 else {
                     $errorFlag = 1;
@@ -32,11 +32,12 @@ mysql_select_db(DB_DATABASE) or die("database not available");
 
             if ( $errorFlag != 1 ) {
                 $sql      = "CALL verify_and_update_adminprofile('$userName','$_POST[display_name]','$_POST[first_name]','$_POST[last_name]','$_POST[about_me]','$_POST[web]')";
-                $result   = mysql_query($sql);
-                if (mysql_error() ) {
+
+                $result   = $conn->query($sql);
+                if ( $conn->error ) {
                     $errorFlag = 1;
                 } else {
-                    if( mysql_result($result, 0) == 0 ) {
+                    if( $result->data_seek(0) == 0 ) {
                         $errorFlag = 1;
                     }
                 }
