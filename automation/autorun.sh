c04e265190989f9c433352669532d0f3d8e5f547
abuzar hamza <abuzarhamza@gmail.com>
2014-05-01 01:12:46 +0530

    added help and usage msg.

--- automation/autorun.sh
+++ automation/autorun.sh
@@ -1,18 +1,32 @@
 #/bin/bash
 
-#checkning the number of argument
-if test $# -ne 1
- then
+
+help_message() {
   echo "Try '$0 --help' for more information."
+  exit 1
+}
+
+#checkning the number of argument
+if test $# -ne 1; then
+ help_message
+fi
+
+
+#checkhing the provided argument
+if test "$1" = "--help"  ; then
+  echo "Usage : $0 devbox\nor : $0 prodbox"
   exit 1
 fi
 
-#checkhing the provided argument
-if test $1=="--help"
- then
-  echo "Usage : $0 devbox\nor : $0 prod"
-fi
+BOX_ENV=$1
 
+case $BOX_ENV in
+   "devbox")
+            ;;
 
+   "prodbox")
+             ;;
 
+   *) help_message ;;
+esac
 
