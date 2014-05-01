ac417d9ed45442cf6f3c23fb33f048dd9e20b82e
abuzar hamza <abuzarhamza@gmail.com>
2014-05-01 18:00:12 +0530

    added some function to check for development setup for dev box

--- automation/autorun.sh
+++ automation/autorun.sh
@@ -1,10 +1,53 @@
 #/bin/bash
 
 
-help_message() {
+help_message () {
   echo "Try '$0 --help' for more information."
   exit 1
 }
+
+status_apache () {
+  testrun "$XAMPP_ROOT/logs/httpd.pid" httpd
+  if  [ $? -eq 1 ] ; then
+    return 1
+   else
+    return 0
+  fi
+}
+
+status_mysql () {
+
+  testrun "$XAMPP_ROOT/var/mysql/$(hostname).pid" mysqld
+  if [ $? -eq 1 ] ; then
+     return 1
+    else
+     return 0
+  fi
+}
+
+testrun () {
+  if test -f $1 ; then
+      pid=`cat $1`
+      if ps ax 2>/dev/null | egrep "^ *$pid.*$2" > /dev/null; then
+          return 1
+         else
+          return 0
+      fi
+  else
+    return 0
+  fi
+}
+
+check_perlmodule() {
+
+  `perl -e 'use $1'`
+  if [ $? -eq 0] ; then
+   return 1
+  else
+    return 0
+  fi
+}
+
 
 #checkning the number of argument
 if test $# -ne 1; then
@@ -19,9 +62,12 @@ if test "$1" = "--help"  ; then
 fi
 
 BOX_ENV=$1
+XAMPP_ROOT=""
 
+#checking the provided argumant || can remove the above if
 case $BOX_ENV in
    "devbox")
+            XAMPP_ROOT="/opt/lampp"
             ;;
 
    "prodbox")
@@ -29,4 +75,26 @@ case $BOX_ENV in
 
    *) help_message ;;
 esac
+
+
+#check the status of apache
+status_apache
+if [ $? -eq 1 ] ; then
+   printf "apache is up\n"
+  else
+   printf "apache server is not up\n"
+   exit 1
+fi
+
+#check the status of mysql
+status_mysql
+if [ $? -eq 1 ] ; then
+   printf "mysql service is up\n"
+  else
+   printf "mysql service is not up\n"
+   exit 1
+fi
+
+#check the status of perl module
+
 
