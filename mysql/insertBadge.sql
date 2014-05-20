36eef52f49f47c9022fc263593356740cf35f5eb
abuzar hamza <abuzarhamza@gmail.com>
2014-05-20 14:59:53 +0530

    removed newline character.

--- mysql/insertBadge.sql
+++ mysql/insertBadge.sql
@@ -1,160 +1,119 @@
-
-
 insert into badge (name,description,type)
  values ('Editor','First edit','bronze');
-
 
 insert into badge (name,description,type)
  values ('Supporter','First up vote','bronze');
 
-
 insert into badge (name,description,type)
  values ('Student','Asked first question with at least one up vote','bronze');
-
 
 insert into badge (name,description,type)
  values ('Teacher','Answered first question with at least one up vote','bronze');
 
-
 insert into badge (name,description,type)
  values ('Popular Question','Asked a question with 1,000 views','bronze');
-
 
 insert into badge (name,description,type)
  values ('Scholar','First accepted answer on your own question','bronze');
 
-
 insert into badge (name,description,type)
  values ('Commentator','Left 10 comments','bronze');
-
 
 insert into badge (name,description,type)
  values ('Yearling','Active member for a year, earning at least 200 reputation','silver');
 
-
 insert into badge (name,description,type)
  values ('Autobiographer','Completed all user profile fields','bronze');
-
 
 insert into badge (name,description,type)
  values ('Nice Answer','Answer voted up 10 times','bronze');
 
-
 insert into badge (name,description,type)
  values ('Notable Question','Asked a question with 2,500 views','silver');
-
 
 insert into badge (name,description,type)
  values ('Nice Question','Question voted up 10 times','bronze');
 
-
 insert into badge (name,description,type)
  values ('Critic','First down vote','bronze');
-
 
 insert into badge (name,description,type)
  values ('Organizer','First retag','bronze');
 
-
 insert into badge (name,description,type)
  values ('Enthusiast','Visited the site each day for 30 days','silver');
-
 
 insert into badge (name,description,type)
  values ('Necromancer','Answered a question more than 60 days later with at least 5 votes','silver');
 
-
 insert into badge (name,description,type)
  values ('Enlightened','First answer was accepted with at least 10 up votes','silver');
-
 
 insert into badge (name,description,type)
  values ('Civic Duty','Voted 300 times','silver');
 
-
 insert into badge (name,description,type)
  values ('Self-Learner','Answered your own question with at least 3 up votes','bronze');
-
 
 insert into badge (name,description,type)
  values ('Famous Question','Asked a question with 5,000 view','gold');
 
-
 insert into badge (name,description,type)
  values ('Taxonomist','Created a tag used by 50 questions','silver');
-
 
 insert into badge (name,description,type)
  values ('Citizen Patrol','First flagged post','bronze');
 
-
 insert into badge (name,description,type)
  values ('Fanatic','Visited the site each day for 100 days','gold');
-
 
 insert into badge (name,description,type)
  values (' Cleanup','First rollback','bronze');
 
-
 insert into badge (name,description,type)
  values ('Good Question','Question voted up 25 times','silver');
-
 
 insert into badge (name,description,type)
  values ('Mortarboard','Hit the daily reputation cap for the first time','bronze');
 
-
 insert into badge (name,description,type)
  values ('Pundit','Left a comment with score of 10 or more','silver');
-
 
 insert into badge (name,description,type)
  values (' Favorite Question','Question favorited by 25 users','silver');
 
-
 insert into badge (name,description,type)
  values ('Guru','Accepted answer and voted up 40 times','silver');
-
 
 insert into badge (name,description,type)
  values ('Disciplined','Deleted own post with score of 3 or higher','silver');
 
-
 insert into badge (name,description,type)
  values ('Strunk & White','Edited 100 entries','silver');
-
 
 insert into badge (name,description,type)
  values ('Peer Pressure','Deleted own post with score of -3 or lower','bronze');
 
-
 insert into badge (name,description,type)
  values ('Great Question','Question voted up 100 times','gold');
-
 
 insert into badge (name,description,type)
  values ('Legendary','Hit the daily reputation cap on 150 days','gold');
 
-
 insert into badge (name,description,type)
  values ('Reversal','Provided answer of +20 score to a question of -5 score','bronze');
-
 
 insert into badge (name,description,type)
  values ('Populist','Provided an answer that outscored an accepted answer with 10 votes by 2x','bronze');
 
-
 insert into badge (name,description,type)
  values ('Stellar Question','Question favorited by 100 users','bronze');
-
 
 insert into badge (name,description,type)
  values ('Great Answer','Answer voted up 100 times','bronze');
 
-
 insert into badge (name,description,type)
  values ('Beta Tester','Participated in the BioStar public beta test. Thank you!','bronze');
-
 
 insert into badge (name,description,type)
  values ('Epic','Hit the daily reputation cap on 50 days','silver');
